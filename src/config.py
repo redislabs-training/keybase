c1a309e3431ec4a03259e5befcadc30ad9358e78
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-05-09 19:49:57 +0200

    cleanup

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 
 REDIS_CFG = {
-    "host" : "localhost",
-    "port" : 6379,
-    "password" : ""
+    "host" : "redis-12189.c55.eu-central-1-1.ec2.cloud.redislabs.com",
+    "port" : 12189,
+    "password" : "hjvbjshfbkuvysbkufdhbvushbudfybvusdf"
 } 
 
