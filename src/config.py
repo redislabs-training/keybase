b9b1b6bc38f56cc4a1a5ab227102597755aafb5e
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-05-09 19:45:14 +0200

    created .gitignore

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 
 REDIS_CFG = {
-    "host" : "localhost",
-    "port" : 4321,
-    "password" : ""
+    "host" : "redis-12189.c55.eu-central-1-1.ec2.cloud.redislabs.com",
+    "port" : 12189,
+    "password" : "FlwotDxjYryWUDJzyFOWyNW2n3ou0h7n"
 } 
 
