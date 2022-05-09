3d308f330ec8674a8d22722cb72ca4f01fc33257
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-05-09 20:48:43 +0200

    credentials

--- src/config.py
+++ src/config.py
@@ -1,7 +1,5 @@
-
 REDIS_CFG = {
-    "host" : "redis-12189.c55.eu-central-1-1.ec2.cloud.redislabs.com",
-    "port" : 12189,
-    "password" : "hjvbjshfbkuvysbkufdhbvushbudfybvusdf"
+    "host" : "localhost",
+    "port" : 6379,
+    "password" : ""
 } 
-
