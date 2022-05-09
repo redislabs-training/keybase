1c5e5b62a8d55c73b2067363a19bb8a3dc12bcae
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-05-09 19:46:22 +0200

    Update config.py
    default connection

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 
 REDIS_CFG = {
-    "host" : "redis-12189.c55.eu-central-1-1.ec2.cloud.redislabs.com",
-    "port" : 12189,
-    "password" : "FlwotDxjYryWUDJzyFOWyNW2n3ou0h7n"
+    "host" : "localhost",
+    "port" : 6379,
+    "password" : ""
 } 
 
