62460465bd4e66f88b84f5259a048b4651b23663
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-10-08 00:15:56 +0200

    Clean up

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export FLASK_APP=src/application.py
 export FLASK_DEBUG=1
-export PYTHONPATH=src/:src/common/
-#flask run --port=443 --cert=adhoc --host=0.0.0.0 &
-#gunicorn --workers 4 --bind 0.0.0.0:5000 "wsgi:create_app()"
+export PYTHONPATH=src/
+
+#gunicorn --workers 1 --bind 0.0.0.0:5000 "wsgi:create_app()"
 flask run --host=0.0.0.0 &
