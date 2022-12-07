e42fbff45efc6f2540dc4382877806adc8749c9f
Mirko Ortensi <mirko.ortensi@gmail.com>
2022-12-07 17:30:09 +0100

    Improved the Version class

--- src/version/version.py
+++ src/version/version.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from redis_om import (EmbeddedJsonModel, Field)
 
+from src.common.config import get_db
+
 
 class Version(EmbeddedJsonModel):
     name: str
@@ -11,4 +13,5 @@ class Version(EmbeddedJsonModel):
     owner: str
 
     class Meta:
+        database = get_db()
         embedded = True
