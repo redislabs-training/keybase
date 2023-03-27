28aae8f280c48997dd399c0da47d5b2b7626033e
David Maier <david.maier@redis.com>
2023-03-27 13:01:43 +0200

    Externalized environment variables for debugging purposes

--- src/common/config.py
+++ src/common/config.py
@@ -12,13 +12,17 @@ REDIS_CFG = {   "host" : os.getenv('DB_SERVICE'),
                 "ssl_ca_certs" : ''} 
 SIGNUP_CFG = 'debug'
 
+# Environment variables
 OKTA_BASE=os.getenv('OKTA_BASE')
 OKTA_CALLBACK_URL=os.getenv('OKTA_CALLBACK_URL')
+OKTA_CLIENT_ID=os.getenv('OKTA_CLIENT_ID')
+OKTA_CLIENT_SECRET=os.getenv('OKTA_CLIENT_SECRET')
+OKTA_API_TOKEN=os.getenv('OKTA_API_TOKEN')
 
 okta = {
-    "client_id": os.getenv('OKTA_CLIENT_ID'),
-    "client_secret": os.getenv('OKTA_CLIENT_SECRET'),
-    "api_token" : os.getenv('OKTA_API_TOKEN'),
+    "client_id": OKTA_CLIENT_ID,
+    "client_secret": OKTA_CLIENT_SECRET,
+    "api_token" : OKTA_API_TOKEN,
     "auth_uri": "https://{}/oauth2/default/v1/authorize".format(OKTA_BASE),
     "token_uri": "https://{}/oauth2/default/v1/token".format(OKTA_BASE),
     "issuer": "https://{}/oauth2/default",
