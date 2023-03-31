ee0fd4cc8c9ef3d790cac982f51f3bcc12af5687
Mirko Ortensi <mirko.ortensi@gmail.com>
2023-03-31 09:29:49 +0200

    indexes creation at startup

--- src/application.py
+++ src/application.py
@@ -5,9 +5,13 @@ from flask_cors import CORS
 from datetime import datetime
 from flask_breadcrumbs import Breadcrumbs
 from werkzeug.exceptions import HTTPException
-import redis
+
 import logging
-from src.common.utils import track_errors
+from src.common.utils import track_errors, get_db
+
+import redis
+from redis.commands.search.field import TextField, TagField, VectorField
+from redis.commands.search.indexDefinition import IndexDefinition
 
 
 def create_app():
@@ -17,6 +21,18 @@ def create_app():
     app.config.update({'SECRET_KEY': secrets.token_hex(64)})
     app.url_map.strict_slashes = False
     CORS(app)
+
+    if not "user_idx" in get_db().execute_command("FT._LIST"):
+        app.logger.info("The index user_idx does not exist, creating it")
+        index_def = IndexDefinition(prefix=["keybase:okta"])
+        schema = (TextField("name"), TagField("group"))
+        get_db().ft('user_idx').create_index(schema, definition=index_def)
+
+    if not "vss_idx" in get_db().execute_command("FT._LIST"):
+        app.logger.info("The index vss_idx does not exist, creating it")
+        index_def = IndexDefinition(prefix=["keybase:vss"])
+        schema = (TagField("state"), TagField("privacy"), VectorField("content_embedding", "HNSW", {"TYPE": "FLOAT32", "DIM": 768, "DISTANCE_METRIC": "L2"}))
+        get_db().ft('vss_idx').create_index(schema, definition=index_def)
 
     @app.template_filter('ctime')
     def timectime(s):
